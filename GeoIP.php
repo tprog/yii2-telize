80d829c7ec7c23b20dbc8a374d245d6e20496488
Artem Bondarenko <a.bondarenko@cronagency.com>
2015-04-08 11:50:21 +0300

    no message

--- GeoIP.php
+++ GeoIP.php
@@ -3,10 +3,13 @@
  * GeoIP.php
  * @author Artem Bondarenko taxist0@gmail.com
  */
-namespace tprog\telize;
+namespace tprog\yii\telize;
 
 class GeoIP extends \yii\base\Component
 {
+	public function test(){
+	 	return "Hello";
+	}
 
 
 
